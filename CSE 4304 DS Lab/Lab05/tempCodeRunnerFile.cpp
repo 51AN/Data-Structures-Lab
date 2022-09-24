71ce0faec54f0a06e6cfa516c9123e9d6c92f7fd
51AN <sianashsad@iut-dhaka.edu>
2022-09-24 21:35:26 +0600

    ?

--- CSE 4304 DS Lab/Lab05/tempCodeRunnerFile.cpp
+++ CSE 4304 DS Lab/Lab05/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-    // while(heap_minimim(a)<=k)
-    // {
-    //     int temp = 0;
-    //     temp = heap_extractmin(a,n) + (2 * heap_extractmin(a,(n-1)));
-    //     minHeapInsert(temp,a,(n-2));
-    //     --n;
-    //     ++cnt;
-    // }
+    {
